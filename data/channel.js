5df2ba68514a574d3c6447157e46dd577a764d1a
MOB-EXTRA <marcjohnfordc@gmail.com>
2026-08-07 01:33:49 +0800

    Restore PTB Hub site from local backup

---
+++ data/channel.js
@@ -0,0 +1,17 @@
+const channelData = [
+  {
+    name: "MOB Mundane",
+    url: "https://www.youtube.com/channel/UCy94xzTQg2fTBQz4LD9ZBoA",
+    logo: "assets/images/MOBMundane.png"
+  },
+  {
+    name: "Vile Tempest Official",
+    url: "https://www.youtube.com/channel/UCbDtYZS08VvB6luAcyn08bQ",
+    logo: "assets/images/VileTempestOfficial.png"
+  },
+  {
+    name: "Infreenite MOB Solutions",
+    url: "https://www.youtube.com/channel/UCjUlbQgLin9oQPiyG6l4z-g",
+    logo: "assets/images/InfreeniteMOBSolutions.png"
+  }
+];
---
+++ data/notarobot.js
@@ -0,0 +1,6 @@
+const notARobot = {
+    code: "1234",
+    
+    // Both the link and the embedded video will pull this exact source:
+    codeSource: "https://youtu.be/Tjf7v4LvU1g"
+};
